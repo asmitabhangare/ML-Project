cafbd3684b0f7292219f921dd11890f3af5587c3
Asmita Bhangare <asmitabhangare01@gmail.com>
2024-10-03 22:23:39 +0530

    ingestion transformation and model training

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -8,6 +9,8 @@ def error_message_detail(error,error_detail:sys):
 
     return error_message
 
+    
+
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
@@ -15,3 +18,4 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
